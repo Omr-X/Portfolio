2436a2a6fa82022528b2e2c56586e47653c51e45
Omar <omarbasfaou@gmail.com>
2026-05-03 22:20:20 -0400

    fix path issue (again)

--- docs/KotlinProject-composeApp.mjs
+++ docs/KotlinProject-composeApp.mjs
@@ -1,6 +1,6 @@
 
 
-import { importObject, setWasmExports } from 'KotlinProject-composeApp.import-object.mjs'
+import { importObject, setWasmExports } from './KotlinProject-composeApp.import-object.mjs'
 
 let wasmInstance;
 
